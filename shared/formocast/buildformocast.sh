f7574c5f211a94e081c818901327c933f5b05479
Peter <Peter.Cheng@amd.com>
2025-12-11 01:45:13 -0600

    buildscript runs bench

--- shared/formocast/buildformocast.sh
+++ shared/formocast/buildformocast.sh
@@ -4,13 +4,17 @@ set -euo pipefail
 usage() {
     cat >&2 <<'EOF'
 Usage:
-  buildformocast.sh [pool <formocast|origami_sk0_ntab0>] [--build_dir <path>] [--rocm <path>] --arch <gfx950>] --prepare]
+  buildformocast.sh [pool <formocast|origami_sk0_ntab0>] [--build_dir <path>] [--rocm <path>] --arch <gfx950>] --prepare] --run_bench] --logs_dir <path>] --device <gpuid>]
+
 Options:
   pool                     Solution pool. Defaults to formocast.
   -a, --arch <arch>        Specify gpu arch. Default to gfx950
   -b, --build_dir <path>   Build directory (optional). Defaults to ./build_\${pool}
   --rocm <path>            Specify rocm-libraries path. Default to ./rocm-libraries
   --prepare                Handling rocm-libraries downloads. Default to false
+  --run_bench              Runing bf16 bench. Default to false
+  --logs_dir <path>        Directory for bench outputs. Default to ./logs
+  --device <gpuid>         GPU id for running bench. Default to 0
   --help                   Print this.
 
 Examples:
@@ -26,6 +30,9 @@ user_dir_set=false
 pool='formocast'
 arch='gfx950'
 prepare=false
+logs_dir='./logs'
+run_bench=false
+device=0
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -39,6 +46,14 @@ while [[ $# -gt 0 ]]; do
                 usage
             fi
             rocm="$2"
+            shift 2
+            ;;
+        --device)
+            if [[ $# -lt 2 ]]; then
+                echo "Error: '$1' requires a value." >&2
+                usage
+            fi
+            device="$2"
             shift 2
             ;;
         -a|--arch)
@@ -71,6 +86,18 @@ while [[ $# -gt 0 ]]; do
 	    prepare=true
 	    shift
 	    ;;
+	--run_bench)
+	    run_bench=true
+	    shift
+	    ;;
+        --logs_dir)
+            if [[ $# -lt 2 ]]; then
+                echo "Error: '$1' requires a value." >&2
+                usage
+            fi
+            logs_dir="$2"
+            shift 2
+            ;;
 	formocast|origami_sk0_ntab0)
 	    pool="$1"
 	    shift
@@ -90,6 +117,7 @@ if [[ "$user_dir_set" == false ]]; then
 fi
 rocm=$(realpath $rocm)
 build_dir=$(realpath $build_dir)
+logs_dir=$(realpath $logs_dir)
 
 if [[ "$prepare" == true ]]; then
     echo "Downloading Formocast Testing Repo into $rocm..."
@@ -114,5 +142,21 @@ cmake --build "$build_dir" --parallel
 popd
 
 echo "Build $build_dir done."
+if [[ "$run_bench" == true ]]; then
+    echo "Logs dir:     $logs_dir"
+    mkdir -p "$logs_dir"
+fi
+
 echo "formocast bench command: TENSILE_PREDICTION_ALGO=1 TENSILE_PREDICTION_LIB=1 ${build_dir}/clients/hipblaslt-bench --yaml <bench.yaml> --device <gpuid>"
+if [[ "$run_bench" == true ]]; then
+    echo "Running formocast with $pool pool on BBSTN bench set and output to ${logs_dir}/formocast_pr_test_251211_bbstn_formocastP${pool}.log"
+    TENSILE_PREDICTION_ALGO=1 TENSILE_PREDICTION_LIB=1 ${build_dir}/clients/hipblaslt-bench --yaml ${rocm}/shared/formocast/gridtest.yaml --device $device > ${logs_dir}/formocast_pr_test_251211_bbstn_formocastP${pool}.log 2>&1
+    echo "Done."
+fi
+
 echo "origami bench command: ${build_dir}/clients/hipblaslt-bench --yaml <bench.yaml> --device <gpuid>"
+if [[ "$run_bench" == true ]]; then
+    echo "Running origami with $pool pool on BBSTN bench set and output to ${logs_dir}/formocast_pr_test_251211_bbstn_origamiP${pool}.log"
+    ${build_dir}/clients/hipblaslt-bench --yaml ${rocm}/shared/formocast/gridtest.yaml --device $device > ${logs_dir}/formocast_pr_test_251211_bbstn_origamiP${pool}.log 2>&1
+    echo "Done."
+fi
